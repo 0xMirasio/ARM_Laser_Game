d8d98ae41157ebefc775cc82dbce04a8fbdc71d8
CelestinML <celestin.moenne@gmail.com>
2020-05-27 12:59:37 +0200

    Commentaires et structure propre du principal.c

--- Projet final/Src/principal.c
+++ Projet final/Src/principal.c
@@ -1,36 +1,40 @@
 #include "gassp72.h"
+#include "etat.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include "etat.h"
 
 #define pwn_periode 360 // KhZ
 #define SYSTICK_PER 360000 // (360000 ticks équivaut à 5ms)
 #define M2TIR 984064 
 
+//On va stocker toutes les informations sur le son dans la variable etat
 type_etat etat;
+//Fonction définie dans Son.s
 extern void callback_son(void);
 extern short Son;
+//On récupère la période et la longueur du son dans le fichier de son en asm obtenu via Wav2asm
+//Ici, les infos sont donc stockées dans bruitverre.s
 extern int LongueurSon;
 extern int PeriodeSonMicroSec;
 extern int dft(short* signal, int k);
-extern int TabSig[];
 
-
-
-
-// CONSTANTES -----------------------
+//Définition des variables
+//Ce tableau va contenir les valeurs des 64 échantillons du signal recu, dès qu'on fait une acquisition
 short int DMA[64];
-int val_k[] = {17 , 18 ,19 ,20 ,23 ,24};
+//On définie un tableau pour donner la correspondance entre chaque signal et sa fréquence
+int val_k[] = {17 ,18 ,19 ,20 ,23 ,24};
+//Tableaux pour compter les occurences d'un signal dans un échantillon, et les scores
 int compteur[] = {0,0,0,0,0,0};
 int score[] = {0,0,0,0,0,0};
 
-
+//On peut paramétrer la trame que l'on souhaite analyser via cette variable
+//Avec la valeur 0x33, on observera le signal 1 (1x1, 2x2, 3x3, 4x4, 5x5 et 0x6)
+//avec une amplitude de 248 et un bruit de fond d'amplitude de 4
 int trame = 0x33; //Scores théoriques : 1, 2, 3, 4, 5, 0
 
-
+//Fonction qui initialise le timer pour le son, ainsi que la fonction de callback associée
 void init_son() {
-	
 	etat.resolution = PWM_Init_ff( TIM3, 3, pwn_periode );
 	etat.son = &Son;
 	etat.taille = LongueurSon;
@@ -39,17 +43,19 @@ void init_son() {
 	
 	Timer_1234_Init_ff( TIM4, etat.periode_ticks );
 	Active_IT_Debordement_Timer( TIM4, 2, callback_son );
-	etat.position = etat.taille; // pour injectez du silence tant qu'un tir n'est pas detecté
+	etat.position = etat.taille; //Pour injectez du silence par défaut tant qu'un tir n'est pas detecté
 	Run_Timer( TIM4 );
-
 }
 
+//Cette fonction sera régulièrement appelée par le timer pour faire une acquisition de 64 points du signal
+//et effectuer une dft dessus pour savoir quel joueur a touché une cible
 void sys_callback(void) {
-	
+	//Lancement de l'acquisition
 	Start_DMA1(64);
 	Wait_On_End_Of_DMA1();
 	Stop_DMA1;
 	
+	//Analyse de l'acquisition
 	for (int i=0; i<6 ; i++) {
 			if (dft(DMA, val_k[i]) > M2TIR) {
 					compteur[i]++;
@@ -67,11 +73,10 @@ void sys_callback(void) {
 }
 int main(void)
 {
-	
-
-
+	//On ne configure qu'une seule fois la CLOCK pour tous les timers
 	CLOCK_Configure();
 	
+	//Configuration du timer principal
 	Init_TimingADC_ActiveADC_ff( ADC1, trame );
 	Single_Channel_ADC( ADC1, 2 );
 	Init_Conversion_On_Trig_Timer_ff( ADC1, TIM2_CC2, 225 );
@@ -81,8 +86,8 @@ int main(void)
 	SysTick_On;
 	SysTick_Enable_IT;
 	
+	//Configuration du timer pour le son
 	init_son();
-
 
 	while	(1) {}
 }
